bc185b955ce7a6ebad47c4f8e43501ce1740b8e1
Christopher Fagiani <christopher.fagiani@gmail.com>
2021-12-06 06:32:56 -0500

    day6: track ages in map

--- src/days/day6.rs
+++ src/days/day6.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use crate::days::Day;
 use crate::input::get_data_from_file;
 
@@ -15,20 +17,29 @@ impl Day for Day6 {
     }
 }
 
-fn run_generations(input_root: &str, generations: i32) -> usize {
-    let fish_string = get_data_from_file(input_root, "test.txt", |s| s);
-    let mut fish_ages: Vec<i32> = fish_string.get(0).unwrap().split(",").map(|c| c.parse().unwrap()).collect();
+fn run_generations(input_root: &str, generations: i32) -> u64 {
+    let fish_string = get_data_from_file(input_root, "day6.txt", |s| s);
+    let fish_ages: Vec<u8> = fish_string.get(0).unwrap().split(",").map(|c| c.parse().unwrap()).collect();
+    let mut age_map: HashMap<u8, u64> = HashMap::new();
+    // create map of number of fish with age days left in their generation time
+    for age in fish_ages {
+        *age_map.entry(age).or_insert(0) += 1
+    }
+
     for _ in 0..generations {
-        let mut new_fish: Vec<i32> = Vec::new();
-        for fish in fish_ages {
-            if fish == 0 {
-                new_fish.push(6);
-                new_fish.push(8);
+        let mut next_gen: HashMap<u8, u64> = HashMap::new();
+        for age in age_map.clone().keys() {
+            let count = *age_map.get(age).unwrap();
+            if *age == 0 {
+                // for all with age of 0, spawn new fish and reset the existing fish age
+                *next_gen.entry(6).or_insert(0) += count;
+                *next_gen.entry(8).or_insert(0) = count;
             } else {
-                new_fish.push(fish - 1);
+                // for all with more time in generation, decrement
+                *next_gen.entry(age - 1).or_insert(0) += count;
             }
         }
-        fish_ages = new_fish
+        age_map = next_gen.clone();
     }
-    return fish_ages.len();
+    return age_map.into_values().collect::<Vec<u64>>().iter().sum::<u64>();
 }
